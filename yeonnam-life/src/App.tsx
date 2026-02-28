c2df15aec653562a6cb565d3528b1cd4540cf03f
Liebe_Chaeso <d131215@naver.com>
2026-02-28 12:12:05 +0900

    chore: 카카오맵 api 테스트 코드 작성, axios 설치

--- yeonnam-life/src/App.tsx
+++ yeonnam-life/src/App.tsx
@@ -1,9 +1,11 @@
+import KakaoMap from "./components/test/kakaoMap";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="w-300px h-500px bg-amber-500 text-white">안녕하세요</div>
+      <div className="w-75 h-75 bg-amber-500 text-white">Hello World</div>
+      <KakaoMap />
     </>
   );
 }
---
+++ yeonnam-life/src/components/test/KakaoMap.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useEffect } from "react";
+
+const KakaoMap = () => {
+  useEffect(() => {
+    window.kakao.maps.load(() => {
+      const container = document.getElementById("map");
+      const options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      if (container) {
+        new window.kakao.maps.Map(container, options);
+      }
+    });
+  }, []);
+
+  return (
+    <div id="map" className="w-75 h-75">
+      test
+    </div>
+  );
+};
+
+export default KakaoMap;
