e71bb70dbb59e597a3bcc35fbc62ad4db785d114
Liebe_Chaeso <d131215@naver.com>
2026-02-28 09:43:10 +0900

    chore: 프로젝트 세팅-리액트 라우터 돔, 테일윈드 css

--- yeonnam-life/src/App.tsx
+++ yeonnam-life/src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="w-300px h-500px bg-amber-500 text-white">안녕하세요</div>
+    </>
+  );
 }
 
 export default App;
