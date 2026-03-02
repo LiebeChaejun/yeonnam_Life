949c7d60c0e7d56057d25b5d7108bbf8a6cff37d
Liebe_Chaeso <d131215@naver.com>
2026-03-02 15:14:50 +0900

    chore: 폴더 구조 정리

--- yeonnam-life/src/App.tsx
+++ yeonnam-life/src/App.tsx
@@ -1,12 +1,11 @@
-import KakaoMap from "./components/test/kakaoMap";
+import Home from "./pages/home/Home";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="w-75 h-75 bg-amber-500 text-white">Hello World</div>
-      <KakaoMap />
-    </>
+    <div className="w-screen h-screen bg-[#F6F3EE] justify-items-center content-center">
+      <Home />
+    </div>
   );
 }
 
---
+++ yeonnam-life/src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <>
+      <div className="bg-[#000] w-[300px] h-[300px]"></div>
+    </>
+  );
+};
+
+export default Home;
