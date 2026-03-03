a8eab93aec5b3d0610e5acddb21e6e4d21299ee1
Liebe_Chaeso <d131215@naver.com>
2026-03-03 21:07:46 +0900

    feat/home: 홈 레이아웃 수정

--- yeonnam-life/src/App.tsx
+++ yeonnam-life/src/App.tsx
@@ -1,12 +1,13 @@
 import Home from "./pages/home/Home";
 // import Place from "./pages/place/Place";
+// import Map from "./pages/map/Map";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="bg-bg w-full min-h-screen flex justify-center">
-        <div className="w-125 py-5 px-3">
+        <div className="w-125">
           <Home />
         </div>
       </div>
