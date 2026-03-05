525cc972b09bf6ad707ec2232824b3a3a28e70a4
Liebe_Chaeso <d131215@naver.com>
2026-03-05 14:30:21 +0900

    feat/course: course 컴포넌트 작업

---
+++ yeonnam-life/src/components/course/Course.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Course = () => {
+  return (
+    <>
+      <li className="flex gap-2">
+        <div className="flex flex-col gap-1 justify-center">
+          <div className="w-10 h-10 bg-cafe rounded-full flex items-center justify-center text-white font-semibold">
+            1
+          </div>
+          <hr className="w-0.5 h-full bg-cafe self-center" />
+        </div>
+        <div className="relative flex gap-2 bg-bg-card w-full px-3 py-3 rounded-2xl items-center">
+          <p className="w-15 h-15 bg-bg-warm text-4xl rounded-xl flex justify-center items-center">
+            ☕️
+          </p>
+          <div>
+            <div className="flex gap-1">
+              <h4 className="text-xl font-semibold">연남 살롱</h4>
+              <div className="p-1 rounded-sm bg-bg-warm text-xs text-cafe content-center">
+                카페
+              </div>
+            </div>
+            <p className="text-sm text-text-muted mr-6">
+              빈티지 감성이 물씬 나는 드립커피 전문점
+            </p>
+            <div className="flex gap-2">
+              <p className="text-text-primary text-xs">별점</p>
+              <p className="text-text-secondary text-xs">
+                서울 마포구 연남로 38
+              </p>
+            </div>
+          </div>
+          <div className="absolute top-4 right-4 text-2xl">❤️</div>
+        </div>
+      </li>
+    </>
+  );
+};
+
+export default Course;
--- yeonnam-life/src/pages/course/Course.tsx
+++ yeonnam-life/src/pages/course/Course.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Course = () => {
   return (
-    <div className="flex flex-col gap-3 py-3">
+    <div className="flex flex-col gap-3 py-3 px-3">
       <div className="flex">
         <p>(뒤로가기)</p>
         <h2 className="text-xl font-semibold">산책 코스</h2>
       </div>
 
       {/* 코스 설명 박스 */}
-      <div className="flex flex-col bg-cafe w-125 h-50 rounded-2xl gap-3 justify-center px-5">
+      <div className="flex flex-col bg-cafe w-full h-50 rounded-2xl gap-3 justify-center px-5">
         <p className="text-4xl">☕️</p>
         <div>
           <p className="font-semibold text-2xl text-white">
@@ -33,37 +33,9 @@ const Course = () => {
         {/* 코스별 장소 목룍*/}
         <ul className="flex flex-col gap-5">
           {/* 코스별 장소 */}
-          <li className="flex gap-2">
-            <div className="flex flex-col gap-1 justify-center">
-              <div className="w-10 h-10 bg-cafe rounded-full flex items-center justify-center text-white font-semibold">
-                1
-              </div>
-              <hr className="w-0.5 h-full bg-cafe self-center" />
-            </div>
-            <div className="relative flex gap-2 bg-bg-card w-full px-3 py-3 rounded-2xl items-center">
-              <p className="w-15 h-15 bg-bg-warm text-4xl rounded-xl flex justify-center items-center">
-                ☕️
-              </p>
-              <div>
-                <div className="flex gap-1">
-                  <h4 className="text-xl font-semibold">연남 살롱</h4>
-                  <div className="px-1 py-1 rounded-xl bg-bg-warm text-xs text-cafe content-center">
-                    카페
-                  </div>
-                </div>
-                <p className="text-sm text-text-muted">
-                  빈티지 감성이 물씬 나는 드립커피 전문점
-                </p>
-                <div className="flex gap-2">
-                  <p className="text-text-primary text-xs">별점</p>
-                  <p className="text-text-secondary text-xs">
-                    서울 마포구 연남로 38
-                  </p>
-                </div>
-              </div>
-              <div className="absolute top-4 right-4 text-2xl">❤️</div>
-            </div>
-          </li>
+          <Course />
+          <Course />
+
           <li className="flex gap-2">
             <div className="flex flex-col gap-1 justify-center">
               <div className="w-10 h-10 bg-cafe rounded-full flex items-center justify-center text-white font-semibold">
