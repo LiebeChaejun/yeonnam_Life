243ccb5527367ca400dd19757a03be2201898973
Liebe_Chaeso <d131215@naver.com>
2026-03-06 15:11:46 +0900

    feat/course 디자인 세부사항 업로드

--- yeonnam-life/src/components/course/CourseCard.tsx
+++ yeonnam-life/src/components/course/CourseCard.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import HeartActive from "../img/heartActive.svg";
+import HeartEmpty from "../img/heartInactive.svg";
+import Star from "../img/star.svg";
 
 const CourseCard = () => {
   return (
     <>
       <li className="flex gap-2">
         <div className="flex flex-col gap-1 justify-center">
-          <div className="w-10 h-10 bg-cafe rounded-full flex items-center justify-center text-white font-semibold">
+          <div className="w-8 h-8 bg-cafe rounded-full flex items-center justify-center text-white font-semibold">
             1
           </div>
-          <hr className="w-0.5 h-full bg-cafe self-center" />
         </div>
         <div className="relative flex gap-2 bg-bg-card w-full px-3 py-3 rounded-2xl items-center">
           <p className="w-15 h-15 bg-bg-warm text-4xl rounded-xl flex justify-center items-center">
@@ -16,22 +18,30 @@ const CourseCard = () => {
           </p>
           <div>
             <div className="flex gap-1">
-              <h4 className="text-xl font-semibold">연남 살롱</h4>
-              <div className="p-1 rounded-sm bg-bg-warm text-xs text-cafe content-center">
+              <h4 className="text-xl font-medium">연남 살롱</h4>
+              <div className="p-1 rounded-full bg-bg-warm text-xs text-cafe content-center">
                 카페
               </div>
             </div>
-            <p className="text-sm text-text-muted mr-6">
+            <p className="text-sm text-text-muted w-50 mr-6">
               빈티지 감성이 물씬 나는 드립커피 전문점
             </p>
             <div className="flex gap-2">
-              <p className="text-text-primary text-xs">별점</p>
+              <div className="flex">
+                <img src={Star} alt="" className="w-4 h-4" />
+                <p className="text-xs text-[#FFC107]">4.9</p>
+              </div>
               <p className="text-text-secondary text-xs">
                 서울 마포구 연남로 38
               </p>
             </div>
           </div>
-          <div className="absolute top-4 right-4 text-2xl">❤️</div>
+          <img
+            src={HeartActive}
+            alt="활성화 된 하트"
+            className="absolute top-4 right-4"
+          />
+          {/* <div className="absolute top-4 right-4 text-2xl">❤️</div> */}
         </div>
       </li>
     </>
--- yeonnam-life/src/pages/course/Course.tsx
+++ yeonnam-life/src/pages/course/Course.tsx
@@ -30,44 +30,13 @@ const Course = () => {
         </div>
       </div>
       <div>
-        <h3 className="text-xl">코스 순서</h3>
+        <h3 className="text-shadow-2xs font-semibold mb-5">코스 순서</h3>
         {/* 코스별 장소 목룍*/}
         <ul className="flex flex-col gap-5">
           {/* 코스별 장소 */}
           <CourseCard />
           <CourseCard />
-
-          <li className="flex gap-2">
-            <div className="flex flex-col gap-1 justify-center">
-              <div className="w-10 h-10 bg-cafe rounded-full flex items-center justify-center text-white font-semibold">
-                2
-              </div>
-              <hr className="w-0.5 h-full bg-cafe self-center" />
-            </div>
-            <div className="relative flex gap-2 bg-bg-card w-full px-3 py-3 rounded-2xl items-center">
-              <p className="w-15 h-15 bg-bg-warm text-4xl rounded-xl flex justify-center items-center">
-                🍵
-              </p>
-              <div>
-                <div className="flex gap-1">
-                  <h4 className="text-xl font-semibold">커피 한약방</h4>
-                  <div className="px-1 py-1 rounded-xl bg-bg-warm text-xs text-cafe content-center">
-                    카페
-                  </div>
-                </div>
-                <p className="text-sm text-text-muted">
-                  한옥 느낌의 독특한 분위기, 한약 라떼가 유명
-                </p>
-                <div className="flex gap-2">
-                  <p className="text-text-primary text-xs">별점</p>
-                  <p className="text-text-secondary text-xs">
-                    서울 마포구 연남로 5길 22
-                  </p>
-                </div>
-              </div>
-              <div className="absolute top-4 right-4 text-2xl">❤️</div>
-            </div>
-          </li>
+          <CourseCard />
         </ul>
       </div>
     </div>
