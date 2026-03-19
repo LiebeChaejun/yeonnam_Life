33c2b5d1b5ac6da5fc8fe8db991e0cb962eb83b6
Liebe_Chaeso <d131215@naver.com>
2026-03-19 17:28:06 +0900

    feat/layout/home: 레이아웃 수정

--- yeonnam-life/src/pages/home/Home.tsx
+++ yeonnam-life/src/pages/home/Home.tsx
@@ -35,37 +35,12 @@ const Home = () => {
       </div>
 
       {/* 인기장소 */}
-      <div>
-        <h2>인기 장소🔥</h2>
+      <div className="flex flex-col gap-3">
+        <h2 className="text-xl font-bold">인기 장소🔥</h2>
         <ul className="flex flex-col gap-3">
           <PlaceCard isHome={true} />
           <PlaceCard isHome={true} />
           <PlaceCard isHome={true} />
-
-          {/* <div className="bg-bg-card h-16 rounded-2xl flex items-center justify-between px-3">
-            <div className="flex items-center gap-3">
-              <p className="text-2xl">🍕</p>
-              <div>
-                <p className="font-semibold">연남동 화덕피자</p>
-                <p className="font-light text-sm">
-                  매일 반죽하는 나폴리 스타일 수제 피자
-                </p>
-              </div>
-            </div>
-            <p className="justify-self-end">별점</p>
-          </div>
-          <div className="bg-bg-card h-16 rounded-2xl flex items-center justify-between px-3">
-            <div className="flex items-center gap-3">
-              <p className="text-2xl">☕️</p>
-              <div>
-                <p className="font-semibold">연남 커피</p>
-                <p className="font-light text-sm">
-                  빈티지 감성이 물씬 나는 드립커피 전문점
-                </p>
-              </div>
-            </div>
-            <p className="justify-self-end">별점</p>
-          </div> */}
         </ul>
       </div>
     </>
