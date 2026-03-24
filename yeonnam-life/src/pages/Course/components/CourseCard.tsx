5b471a7282a957a68a331bdc411eb9ec079d00cf
Liebe_Chaeso <d131215@naver.com>
2026-03-24 15:00:46 +0900

    feat/course: 카드 클릭시 '/course'페이지로 navagate 설정

--- yeonnam-life/src/pages/Course/components/CourseCard.tsx
+++ yeonnam-life/src/pages/Course/components/CourseCard.tsx
@@ -1,15 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
 import iconClock from "../../../assets/icons/iconClockGrey.svg";
 import iconWalking from "../../../assets/icons/iconWalkingGrey.svg";
 
 const CourseCard = () => {
+  const navigate = useNavigate();
+  const navigateToCourse = () => {
+    navigate("/course");
+  };
+
   return (
-    <div className="bg-bg-card min-w-70 h-40 rounded-2xl flex flex-col gap-3 px-3 py-5">
+    <button
+      onClick={navigateToCourse}
+      className="bg-bg-card min-w-70 h-40 rounded-2xl flex flex-col gap-3 px-3 py-5"
+    >
       <p className="text-3xl">☕️</p>
       <div className="flex flex-col">
         <p className="font-semibold text-base">연남 감성 카페 투어</p>
         <p className="font-light text-sm">커피 한 잔과 함께 걷는 연남의 오후</p>
       </div>
-      <div className="flex gap-3">
+      <div className="flex justify-center gap-3">
         <div className="flex items-center gap-1">
           <img src={iconClock} alt="" className="w-4 h-4" />
           <p className="text-sm font-light">시간</p>
@@ -19,7 +29,7 @@ const CourseCard = () => {
           <p className="text-sm font-light">거리</p>
         </div>
       </div>
-    </div>
+    </button>
   );
 };
 
