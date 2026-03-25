592af705fe43665597b067441592ce4da79790d5
Liebe_Chaeso <d131215@naver.com>
2026-03-25 10:01:14 +0900

    feat/course: courseid에 맞는 course데이터 불러오기
    작업내용
    - courseId를 쿼리로 붙여줘서 서버에 전달
    - 서버는 해당하는 데이터를 클라이언트에게 전달

--- yeonnam-life/src/pages/Course/CourseDetail.tsx
+++ yeonnam-life/src/pages/Course/CourseDetail.tsx
@@ -1,11 +1,43 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Course from "./components/Course";
+import axios from "axios";
+
 import backArrow from "../../assets/icons/backArrow.svg";
 
+interface Course {
+  id: string;
+  editorid: string;
+  description: string;
+  duration: string;
+  distance: string;
+}
+
 const CourseDetail = () => {
+  const [course, setCourse] = useState({});
+  const { id } = useParams();
+
+  const getCourseDataById = async () => {
+    try {
+      const res = await axios(`http://localhost:3000/courses/${id}`);
+      setCourse(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCourseDataById();
+    console.log(course);
+  }, []);
+
   return (
     <>
       <div className="flex gap-1">
-        <img src={backArrow} alt="" />
+        <button>
+          <img src={backArrow} alt="뒤로가기" />
+        </button>
         <h2 className="text-xl font-semibold">산책 코스</h2>
       </div>
 
