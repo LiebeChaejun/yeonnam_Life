0763fd4dfbaaf0590f942bf47eae86d99f8bdb4e
Liebe_Chaeso <d131215@naver.com>
2026-03-28 10:35:14 +0900

    feat/course: Course 데이터를 state로 관리하기 위한 타입 선언
    작업 내용:
    - optional 대신에 Partial 사용해서 부분 적으로 사용할 수 있도록 정의
    - zustand로 상태를 관리하기에 앞서, zustand에서 사용할 타입들을 정의

---
+++ yeonnam-life/src/types/Courses.ts
@@ -0,0 +1,39 @@
+interface Spot {
+  order: number;
+  placeId: string;
+  note: string;
+}
+
+interface Course {
+  id: string;
+  editorId: string;
+  title: string;
+  description: string;
+  duration: string;
+  distance: string;
+  likeCount: number;
+  spots: Spot[];
+  tags: string[];
+  thumbnail: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+type PartialCourse = Partial<Course>;
+
+export type CourseState = {
+  course: PartialCourse | null; // 단일 코스
+  courses: PartialCourse[]; // 목록
+  isLoading: boolean;
+  error: string | null;
+};
+
+export type CourseActions = {
+  actions: {
+    setCourse: (course: PartialCourse) => void;
+    setCourses: (courses: PartialCourse[]) => void;
+    setLoading: (loading: boolean) => void;
+    setError: (error: string | null) => void;
+    reset: () => void;
+  };
+};
