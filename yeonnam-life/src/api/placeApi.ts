308038e44644cef6aea0905c65015beadf24271d
Liebe_Chaeso <d131215@naver.com>
2026-03-28 14:03:48 +0900

    feat/place: getPlaceById api 작업

--- yeonnam-life/src/api/placeApi.ts
+++ yeonnam-life/src/api/placeApi.ts
@@ -4,3 +4,8 @@ export const getPlaces = async () => {
   const { data } = await api.get("places");
   return data;
 };
+
+export const getPlaceById = async (id: string) => {
+  const { data } = await api.get(`places/${id}`);
+  return data;
+};
