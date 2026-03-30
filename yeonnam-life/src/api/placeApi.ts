df58c507bd23d891571be857e5e5a7cc7453dda2
Liebe_Chaeso <d131215@naver.com>
2026-03-30 09:26:11 +0900

    feat/place placeApi 필요한 기능 추가 및 설명 추가

--- yeonnam-life/src/api/placeApi.ts
+++ yeonnam-life/src/api/placeApi.ts
@@ -1,11 +1,19 @@
 import api from "./index";
 
+// places 목록 api
 export const getPlaces = async () => {
   const { data } = await api.get("places");
   return data;
 };
 
+// place 세부정보 api
 export const getPlaceById = async (id: string) => {
   const { data } = await api.get(`places/${id}`);
+  return data;
+};
+
+// (별점순) place 상위 5개 불러오는 api
+export const getHotPlace = async () => {
+  const { data } = await api.get("hotPlaces");
   return data;
 };
