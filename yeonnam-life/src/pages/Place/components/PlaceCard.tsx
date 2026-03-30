49105aaa5b98cc1adb5c054ac2141a79933a4ca2
Liebe_Chaeso <d131215@naver.com>
2026-03-30 09:43:39 +0900

    feat/place placeCard hover 시 효과 적용 및 border 스타일 추가

--- yeonnam-life/src/pages/Place/components/PlaceCard.tsx
+++ yeonnam-life/src/pages/Place/components/PlaceCard.tsx
@@ -9,45 +9,43 @@ interface PlaceCardProps {
 
 const PlaceCard = ({ place }: PlaceCardProps) => {
   return (
-    <li className="flex gap-2">
-      <div className="relative flex gap-2 bg-bg-card w-full px-3 py-3 rounded-2xl items-center">
-        {/* 이모지 */}
-        <p className="w-15 h-15 bg-bg-warm text-4xl rounded-xl flex justify-center items-center">
-          ☕️
-        </p>
+    <li className="relative flex gap-2 border-border border-2 bg-bg-card w-full px-3 py-3 rounded-2xl items-center hover:bg-bg-warm">
+      {/* 이모지 */}
+      <p className="w-15 h-15 bg-bg-warm text-4xl rounded-xl flex justify-center items-center">
+        ☕️
+      </p>
 
-        {/* 텍스트 정보 */}
-        <div>
-          {/* 이름 + 카테고리 */}
-          <div className="flex gap-1">
-            <h4 className="text-xl font-medium">{place?.name}</h4>
-            <div className="p-1 rounded-full bg-bg-warm text-xs text-cafe content-center">
-              {place?.category}
-            </div>
-          </div>
-
-          {/* 설명 */}
-          <p className="text-sm text-text-muted w-50 mr-6">
-            {place?.shortDescription}
-          </p>
-
-          {/* 별점 + 주소 */}
-          <div className="flex gap-2">
-            <div className="flex">
-              <img src={star} alt="" className="w-4 h-4" />
-              <p className="text-xs text-[#FFC107]">{place.rating}</p>
-            </div>
-            <p className="text-text-secondary text-xs">{place.address}</p>
+      {/* 텍스트 정보 */}
+      <div>
+        {/* 이름 + 카테고리 */}
+        <div className="flex gap-1">
+          <h4 className="text-xl font-medium">{place?.name}</h4>
+          <div className="p-1 rounded-full bg-bg-warm text-xs text-cafe content-center">
+            {place?.category}
           </div>
         </div>
 
-        {/* 하트 */}
-        <img
-          src={heartActive}
-          alt="활성화 된 하트"
-          className="absolute top-4 right-4"
-        />
+        {/* 설명 */}
+        <p className="text-sm text-text-muted w-50 mr-6">
+          {place?.shortDescription}
+        </p>
+
+        {/* 별점 + 주소 */}
+        <div className="flex gap-2">
+          <div className="flex">
+            <img src={star} alt="" className="w-4 h-4" />
+            <p className="text-xs text-[#FFC107]">{place.rating}</p>
+          </div>
+          <p className="text-text-secondary text-xs">{place.address}</p>
+        </div>
       </div>
+
+      {/* 하트 */}
+      <img
+        src={heartActive}
+        alt="활성화 된 하트"
+        className="absolute top-4 right-4"
+      />
     </li>
   );
 };
