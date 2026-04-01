eb3e62589c058216897ebf5f914a7b6dd81a8d22
Liebe_Chaeso <d131215@naver.com>
2026-04-01 09:46:05 +0900

    feat/place: api로 받은 데이터 렌더링 및 스타일 수정

--- yeonnam-life/src/pages/Place/PlaceDetail.tsx
+++ yeonnam-life/src/pages/Place/PlaceDetail.tsx
@@ -1,4 +1,7 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { usePlaceStore } from "@/stores/usePlaceStore";
+import { getPlaceById } from "@/api/placeApi";
 
 import CategoryBox from "../../components/Category/CategoryButton";
 import backArrow from "../../assets/icons/backArrow.svg";
@@ -9,6 +12,28 @@ import location from "../../assets/icons/iconLocationGray.svg";
 
 const PlaceDetail = () => {
   const navigate = useNavigate();
+  const { id: placeId } = useParams<{ id: string }>();
+  const { actions: placeActions } = usePlaceStore();
+
+  const place = usePlaceStore((state) => state.place);
+  const isLoading = usePlaceStore((state) => state.isLoading);
+  const error = usePlaceStore((state) => state.error);
+
+  useEffect(() => {
+    const fetch = async () => {
+      placeActions.setLoading(true);
+      try {
+        const placeData = await getPlaceById(placeId);
+        placeActions.setPlace(placeData);
+      } catch (error) {
+        placeActions.setError("해당 장소 정보를 불러오는데 실패했습니다.");
+      } finally {
+        placeActions.setLoading(false);
+      }
+    };
+    fetch();
+  }, []);
+
   return (
     <>
       {/* 뒤로가기 버튼 */}
@@ -20,45 +45,51 @@ const PlaceDetail = () => {
       </div>
 
       {/* 사진 담는 곳 */}
-      <div className="h-50 rounded-2xl bg-bg-warm border-border border flex justify-center items-center">
+      <div className="h-50 rounded-2xl bg-bg-warm border-border border-2 flex justify-center items-center">
         <p className="text-7xl">🍞</p>
       </div>
 
       {/* 컨텐츠 */}
       <div className="relative h-75 flex flex-col gap-3">
-        <div className="absolute w-12 h-12 bg-bg-warm rounded-full right-0 top-2 flex justify-center items-center">
-          <img src={heart} alt="" />
-        </div>
+        <button className="absolute w-12 h-12 bg-bg-warm rounded-full right-0 top-2 flex justify-center items-center border-border border-2">
+          <img src={heart} alt="좋아요 버튼" />
+        </button>
 
         {/* 카테고리 태그 */}
         <div className="py-2 text-xs flex gap-1">
-          <CategoryBox isButton={false} shop="bakery" />
+          <CategoryBox isButton={false} shop={place?.category} />
         </div>
 
         {/* 컨텐츠 내용 */}
         <div className="flex flex-col gap-5">
-          <h3 className="text-2xl text-text-primary font-semibold">밀도</h3>
-          <p className="text-text-secondary">식빵 한 장의 행복, 연남 본점</p>
-          <ul className="h-30 bg-bg-warm rounded-2xl border border-border p-2 flex flex-col gap-3">
+          <h3 className="text-2xl text-text-primary font-semibold">
+            {place?.name}
+          </h3>
+          <p className="text-text-secondary max-w-100">{place?.description}</p>
+          <ul className="h-30 bg-bg-warm rounded-2xl border-border border-2 p-2 flex flex-col gap-3">
             <li className="flex gap-1 items-center">
-              <img src={star} alt="" className="w-4 h-4" />
-              <p className="text-accent-warm">별점</p>
+              <img src={star} alt="별점" className="w-4 h-4" />
+              <p className="text-accent-warm">{place?.rating}</p>
             </li>
             <li className="flex gap-1 items-center">
               <img src={location} alt="" className="w-4 h-4" />
-              <p className="text-text-secondary">서울 마포구 연남로 13</p>
+              <p className="text-text-secondary">{place?.address}</p>
             </li>
             <li className="flex gap-1 items-center">
               <img src={clock} alt="" className="w-4 h-4" />
-              <p className="text-text-secondary">영업 시간</p>
+              <p className="text-text-secondary">{place?.hours.weekday}</p>
             </li>
           </ul>
 
           {/* 태그 목록 */}
           <div className="flex gap-3">
-            <p className="bg-bg-warm text-text-secondary text-sm rounded-full px-3 py-2">
-              #식빵
-            </p>
+            {place?.tags.map((tag) => {
+              return (
+                <p className="bg-bg-warm text-text-secondary text-sm rounded-full px-3 py-2">
+                  {"#" + tag}
+                </p>
+              );
+            })}
           </div>
         </div>
       </div>
